28a6e5aaac27713e8448014756fe68832fce35b8
weewilsonboy <wilson.euan@gmail.com>
2023-07-18 15:18:34 +0100

    extention complete, no css

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -6,3 +6,7 @@ order_blueprint = Blueprint("orders", __name__)
 @order_blueprint.route("/orders")
 def index():
     return render_template("index.html", title="Order List", order_list = orders)
+
+@order_blueprint.route("/orders/<number>")
+def order(number):
+    return render_template("order.html", title="Order", order = orders[int(number)])
